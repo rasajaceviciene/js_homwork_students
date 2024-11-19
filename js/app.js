7e8a4286819bc5fe3befa198831484ff8457fa4e
rasajaceviciene <r.jaceviciene@gmail.com>
2024-11-19 21:12:59 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,564 @@
+const students = {
+    '7a': [
+        {
+            id: 1,
+            firstName: "Jonas",
+            lastName: "Jasutis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                10,
+                9,
+                10,
+                8,
+                8
+            ]
+        },
+        {
+            id: 2,
+            firstName: "Matas",
+            lastName: "Petraitis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                6,
+                8,
+                9,
+                7,
+                7
+            ]
+        },
+        {
+            id: 3,
+            firstName: "Marija",
+            lastName: "Peleckaite",
+            studentClass: 7,
+            subjects: [
+                "Informatika",
+                "Geografija",
+                "Lietuviu klb",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                9,
+                7,
+                10,
+                8
+            ]
+        },
+        {
+            id: 4,
+            firstName: "Algis",
+            lastName: "Laurinkus",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                8,
+                9,
+                7,
+                8
+            ]
+        },
+        {
+            id: 5,
+            firstName: "Laima",
+            lastName: "Butvilaite",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Informatika",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                6,
+                7,
+                6,
+                7,
+                8
+            ]
+        },
+        {
+            id: 6,
+            firstName: "Svajunas",
+            lastName: "Miezelis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Informatika",
+                "Anglu klb."
+            ],
+            grades: [
+                10,
+                10,
+                10,
+                9,
+                9
+            ]
+        },
+        {
+            id: 7,
+            firstName: "Indre",
+            lastName: "Mikolaityte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Daile",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                8,
+                10,
+                10,
+                9,
+                8
+            ]
+        },
+        {
+            id: 8,
+            firstName: "Gerda",
+            lastName: "Zemaityte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                7,
+                9,
+                8,
+                8,
+                8
+            ]
+        },
+        {
+            id: 9,
+            firstName: "Markas",
+            lastName: "Laurutis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Anglu klb.",
+                "Fizinis lav.."
+            ],
+            grades: [
+                5,
+                7,
+                6,
+                7,
+                8
+            ]
+        },
+        {
+            id: 10,
+            firstName: "Gyte",
+            lastName: "Pangonyte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                9,
+                10,
+                8,
+                9
+            ]
+        }    
+    ],
+    '7b': [
+        {
+            id: 1,
+            firstName: "Nedas",
+            lastName: "Marciulionis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                10,
+                9,
+                9,
+                9
+            ]
+        },
+        {
+            id: 2,
+            firstName: "Guste",
+            lastName: "Luksyte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                7,
+                9,
+                10,
+                8,
+                8
+            ]
+        },
+        {
+            id: 3,
+            firstName: "Austeja",
+            lastName: "Peleckaite",
+            studentClass: 7,
+            subjects: [
+                "Informatika",
+                "Geografija",
+                "Lietuviu klb",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                8,
+                8,
+                8,
+                9,
+                8
+            ]
+        },
+        {
+            id: 4,
+            firstName: "Lukas",
+            lastName: "Mazylis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                10,
+                9,
+                10,
+                8,
+                9
+            ]
+        },
+        {
+            id: 5,
+            firstName: "Jokubas",
+            lastName: "Radvilas",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Informatika",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                7,
+                7,
+                8,
+                7,
+                8
+            ]
+        },
+        {
+            id: 6,
+            firstName: "Ignas",
+            lastName: "Mazutis",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Informatika",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                9,
+                9,
+                10,
+                10
+            ]
+        },
+        {
+            id: 7,
+            firstName: "Indre",
+            lastName: "Leskauskaite",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Daile",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                9,
+                8,
+                9,
+                8
+            ]
+        },
+        {
+            id: 8,
+            firstName: "Ruta",
+            lastName: "Onaityte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Geografija",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                6,
+                7,
+                7,
+                6,
+                8
+            ]
+        },
+        {
+            id: 9,
+            firstName: "Kaja",
+            lastName: "Ulyte",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Anglu klb.",
+                "Fizinis lav.."
+            ],
+            grades: [
+                6,
+                8,
+                7,
+                8,
+                9
+            ]
+        },
+        {
+            id: 10,
+            firstName: "Justas",
+            lastName: "Pilukas",
+            studentClass: 7,
+            subjects: [
+                "Matematika",
+                "Lietuviu klb.",
+                "Fizika",
+                "Istorija",
+                "Anglu klb."
+            ],
+            grades: [
+                9,
+                9,
+                9,
+                8,
+                9
+            ]
+        }    
+    ]
+}
+
+//Uzduoties sprendimas
+
+console.log(`Mokiniai mokosi siose klasese: ${Object.keys(students)}`);
+
+console.log(`-------------------`);
+
+let department = Object.keys(students);
+
+for(let i = 0; i < department.length; i++){    //Iteracija vyks tiek kartu, kiek yra klasiu
+
+    for(let j = 0; j < (students[department[i]]).length; j++){   //Iteracija vyks tiek kartu, kiek yra mokiniu klaseje
+
+        console.log(`${(students[department[i]])[j].id}. Mokinys: "${
+            (students[department[i]])[j].firstName} ${(students[department[i]])[j].lastName}"`);
+
+        console.log(`Klase: ${(students[department[1]])[j].studentClass}`);
+
+        console.log(`-------------------`);
+
+        console.log(`Dalyku sarasas:`);
+
+        for(let k = 0; k < ((students[department[i]])[j].subjects).length; k++){  //Iteracija vyks tiek kartu, kiek pazymiu uz mokomuosius dalykus turi mokinys
+
+            console.log(`${((students[department[i]])[j].subjects)[k]}: ${
+                ((students[department[i]])[j].grades)[k]}`);
+
+        }      
+
+        console.log(`-------------------`);
+
+        let gradesSum = 0;
+
+        for(let k = 0; k < ((students[department[i]])[j].grades).length; k++){  //Iteracija vyks tiek kartu, kiek pazymiu uz mokomuosius dalykus turi mokinys
+
+            gradesSum += ((students[department[i]])[j].grades)[k];
+
+        }  
+ 
+        let gradesCount = (((students[department[i]])[j].grades).length);
+
+        let studentGradesAverage = gradesSum / gradesCount
+   
+        console.log(`Vidurkis: ${studentGradesAverage.toFixed(1)}`); 
+        // Data types => 5.2 Numbers
+        /* isveda stringa, norint isvesti skaiciu reikia + pries studentGradesAverage, 
+        bet tada netenkinamas reikalavimas vieno skaiciaus po kablelio*/
+        
+        console.log(`-------------------`);
+    
+    }
+
+}
+
+
+//----------------------------------------------------------
+
+// Pirminiai variantai
+
+/*
+console.log((Object.values(students)[0]));
+console.log(`Klase: ${(students['7a'])[1].studentClass}`);
+console.log((classA[0].grades).length);
+console.log(((classA[0].grades).reduce((sum, current) => sum + current, 0)) / ((classA[0].grades).length));
+console.log(classA.length);
+*/
+
+/*
+let classA = students['7a'];
+
+let classB = students['7b'];
+
+console.log(`Mokiniai mokosi siose klasese: ${Object.keys(students)}`);
+
+console.log(`-------------------`);
+
+for(let i = 0; i < classA.length; i++){
+    
+    console.log(`${classA[i].id}. Mokinys: "${classA[i].firstName} ${classA[i].lastName}"`);
+        
+    console.log(`Klase: ${classA[i].studentClass}`);
+    console.log(`-------------------`);
+
+    console.log(`Dalyku sarasas:`);
+
+    for(let j = 0; j < (classA[i].subjects).length; j++){
+        console.log(`${(classA[i].subjects)[j]}: ${(classA[i].grades)[j]}`);
+    }
+
+    console.log(`-------------------`);
+
+    console.log(`Vidurkis: ${(((classA[i].grades).reduce((sum, current) => sum + current, 0)) / ((classA[i].grades).length))}`);
+
+    console.log(`-------------------`);
+}
+
+for(let i = 0; i < classB.length; i++){
+    
+    console.log(`${classB[i].id}. Mokinys: "${classB[i].firstName} ${classB[i].lastName}"`);
+        
+    console.log(`Klase: ${classB[i].studentClass}`);
+    console.log(`-------------------`);
+
+    console.log(`Dalyku sarasas:`);
+
+    for(let j = 0; j < (classB[i].subjects).length; j++){
+        console.log(`${(classB[i].subjects)[j]}: ${(classB[i].grades)[j]}`);
+    }
+
+    console.log(`-------------------`);
+
+    console.log(`Vidurkis: ${(((classB[i].grades).reduce((sum, current) => sum + current, 0)) / ((classB[i].grades).length))}`);
+
+    console.log(`-------------------`);
+}
+*/
+/*
+console.log(students['7a']); //students objekto visas 7a propertis su reiksmemis ir properciais viduje
+console.log((Object.keys(students))[0]); // students objekto 1 propertis, t.y. 7a
+console.log(students[((Object.keys(students))[0])]); // tas pats kas console.log(students['7a'])
+console.log(Object.keys(students).length); // objekto students ilgis
+*/
+
+/*console.log(`${(students[department[0]])[0].id}. Mokinys: "${(students[department[0]])[0].firstName} ${(students[department[0]])[0].lastName}"`);
+
+console.log(`Klase: ${(students[department[0]])[0].studentClass}`);
+
+console.log(`-------------------`);
+
+console.log(`Dalyku sarasas:`);
+
+console.log(`${((students[department[0]])[0].subjects)[0]}: ${((students[department[0]])[0].grades)[0]}`)
+
+console.log(`-------------------`);
+
+console.log(`Vidurkis:`)
+
+console.log(`-------------------`);*/
+
+/*
+let totalSum = [
+    ((students[department[0]])[0].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[1].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[2].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[3].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[4].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[5].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[6].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[7].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[8].grades).reduce((sum, current) => sum + current, 0),
+    ((students[department[0]])[9].grades).reduce((sum, current) => sum + current, 0)
+]
+
+console.log(totalSum.reduce((sum, current) => sum + current))
+*/
+
+ //let gradesSum = ((students[department[i]])[j].grades).reduce((sum, current) => sum + current, 0);
+        // Data types => 5.5 Array methods
